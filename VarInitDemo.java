ca78e0de1010d4119036dc4e09894a17c0f64bf2
Ricardo R. Schutz <ricardo.rosa.schutz@gmail.com>
2023-12-22 08:37:42 -0300

    Amarelo

---
+++ VarInitDemo.java
@@ -0,0 +1,17 @@
+//Demonstra o tempo de vida de uma variavel.
+/**
+ * VarInitDemo
+ */
+public class VarInitDemo {
+
+    public static void main(String[] args) {
+        int x;
+
+        for(x=0;x<3;x++){
+            int y = -1; //y sera inicializada sempre que entrarmos no bloco
+            System.out.println("y is now: "+ y);//esta linha sempre exibe -1
+            y=100;
+            System.out.println("y is now: "+y);
+        }
+    }
+}
