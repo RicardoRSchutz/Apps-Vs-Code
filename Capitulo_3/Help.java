e3d71f26efa0ee0eab76bdf08578a8bc104e4a15
Ricardo R. Schutz <ricardo.rosa.schutz@gmail.com>
2024-01-05 08:22:59 -0300

    Help.java

---
+++ Capitulo_3/Help.java
@@ -0,0 +1,44 @@
+/*
+ * Tente isto 3-1
+ * 
+ * Um sistema de ajuda simples.
+ */
+
+package Capitulo_3;
+
+public class Help {
+    public static void main(String[] args) 
+    throws java.io.IOException {
+        char choice;
+
+        System.out.println("Help on:");
+        System.out.println(" 1. if");
+        System.out.println( " 2. switch");
+        System.out.println("Choose one: ");
+        choice = (char) System.in.read();
+
+        System.out.println("\n");
+
+        switch (choice) {
+            case '1':
+            System.out.println("The if:\n");
+            System.out.println("If (condition) statement;");
+            System.out.println("else statemant;");
+            break;
+
+            case '2':
+            System.out.println("The switch:\n");
+            System.out.println("switch(exoression) {");
+            System.out.println("    case constant:");
+            System.out.println("    statement constant:");
+            System.out.println("    break;");
+            System.out.println(" //...");
+            System.out.println("}");
+            break;
+            
+            default:
+            System.out.println("Selection not found.");
+        }
+    }
+    
+}
